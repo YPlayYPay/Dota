8f4ea686bfad344234f16ec3e69f7f4f77b0815a
{keaidaitou} <{1421732174@qq.com}>
2017-02-10 20:01:05 +0800

    slider

--- js/index.js
+++ js/index.js
@@ -3,6 +3,17 @@
  */
 
 $(function(){
-    var imgs =document.getElementsByTagName('img');
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+    var container = document.getElementsByClassName('container')[0];
+    var imgs = container.getElementsByTagName('img');
     $(imgs).addClass('zoom');
-})
+});
